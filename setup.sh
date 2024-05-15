48e68d2b916931d2758e69e8c554e583bc584b73
joonarafael <joona.kettunen21@gmail.com>
2024-05-15 11:25:11 +0300

    first working software

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "Initializing program environment and installing dependencies..."
+
+sudo apt-get update
+sudo apt-get install python3.6 python3-tk
+
+chmod u+x ./run.sh
+
+echo "Dependencies installed..."
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import pathlib
+from tkinter import filedialog
+from pathlib import Path
 import csv
 
-ABSOLUTE_PATH = pathlib.Path().resolve()
+print("Launching program...")
+print("Imports successful.")
 
 species_rows = []
 table = {
@@ -38,21 +40,98 @@ table = {
     31: [],
 }
 
-with open(f'{ABSOLUTE_PATH}/src/input/kopio.csv', newline='') as csvfile:
-    file_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+EXIT_PROMPT = "Exiting program..."
 
-    for row in file_reader:
-        split_by_comma = row[0].split(',')
-        species_rows.append(split_by_comma)
+print("Asking user to select a CSV file..")
+
+chosen_file_path = filedialog.askopenfilename(title = "Select the CSV file", filetypes = (("CSV Files","*.csv"),))
+
+if not chosen_file_path:
+    print("No file was selected.")
+    print(EXIT_PROMPT)
+    exit()
+
+print(f"File '{chosen_file_path}' selected.")
+print("Starting to read the CSV file...")
+
+try:
+    with open(chosen_file_path, newline='') as csv_file:
+        file_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+
+        for row in file_reader:
+            split_by_comma = row[0].split(',')
+            species_rows.append(split_by_comma)
+except Exception as e:
+    print("Run into an exception while reading the given file:")
+    print(e)
+    print("Please ensure the file is a valid CSV file and try again.")
+    print(EXIT_PROMPT)
+    exit()
+
+if (len(species_rows) < 3):
+    print("No data was found in the CSV file.")
+    print(EXIT_PROMPT)
+    exit()
 
 del species_rows[0]
 del species_rows[0]
 
-for row in species_rows:
-    species = row[0]
+print("CSV reading was successful.")
+print(f"Recognized a total of {len(species_rows)} species.")
+print("Starting to process the data...")
 
-    for i, record in enumerate(row):
-        if i > 0 and record == "#N/A":
-            table[i].append(species)
+try:
+    for row in species_rows:
+        species = row[0]
 
-print(table)
+        for i, record in enumerate(row):
+            if i > 0 and record == "#N/A":
+                table[i].append(species)
+except Exception as e:
+    print("Run into an exception while processing the data:")
+    print(e)
+    print("Please ensure the file is a valid CSV file and try again.")
+    print(EXIT_PROMPT)
+    exit()
+
+print("Data processing finished.")
+print("Results are as follows:")
+
+for day in table:
+    if (len(table[day]) != 0):
+        print(f"Day {day:02}: {len(table[day]):03} species")
+
+print("Asking user to select an output destination..")
+
+chosen_dir_path = filedialog.askdirectory(title = "Select the output destination")
+
+try:
+    Path(f"{chosen_dir_path}/tiirascraper").mkdir(parents=True, exist_ok=True)
+    Path(f"{chosen_dir_path}/noformat").mkdir(parents=True, exist_ok=True)
+except Exception as e:
+    print("Run into an exception while creating the output directories:")
+    print(e)
+    print(EXIT_PROMPT)
+    exit()
+
+print("Starting to write the output files...")
+
+try:
+    for day in table:
+        if (len(table[day]) != 0):
+            with open(f"{chosen_dir_path}/tiirascraper/day_{day:02}.txt", "w+") as output_file:
+                for species in table[day]:
+                    output_file.write(f"{species}\n")
+
+            with open(f"{chosen_dir_path}/noformat/day_{day:02}.txt", "w+") as output_file:
+                one_string = ", ".join(table[day])
+                output_file.write(one_string)
+except Exception as e:
+    print("Run into an exception while creating the output directories:")
+    print(e)
+    print(EXIT_PROMPT)
+    exit()
+
+print("File writing successful.")
+print("Program execution finished.")
+exit()
